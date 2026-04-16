3d949ac54285d830e8ca4727b678a93f4b0557e7
azipiagam <azi@piagam.id>
2026-04-16 16:26:46 +0700

    sync user 2

--- backend/app/Http/Controllers/UserManagementController.php
+++ backend/app/Http/Controllers/UserManagementController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 use App\Services\SnipeItService;
+use App\Services\TicketService;
 
 class UserManagementController extends Controller
 {
