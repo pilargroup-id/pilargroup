befa2a9fa4cc19ad1479d33c7188faa58eb4c552
azipiagam <azi@piagam.id>
2026-06-17 13:30:39 +0700

    testing 5

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,20 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { registerSW } from 'virtual:pwa-register'
 import App from './App.jsx'
 import '@/assets/styles/index.css'
 
+// Matikan semua service worker
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.getRegistrations().then((registrations) => {
     registrations.forEach((reg) => reg.unregister())
   })
-  // Clear semua cache SW
   caches.keys().then((keys) => {
     keys.forEach((key) => caches.delete(key))
   })
 }
-
-registerSW({ immediate: true })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
