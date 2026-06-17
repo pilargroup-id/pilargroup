09591f8d37374eae6c83712588e27d24c3cb1bc2
azipiagam <azi@piagam.id>
2026-06-17 14:10:32 +0700

    testing 9

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,20 +4,23 @@ import { registerSW } from 'virtual:pwa-register'
 import App from './App.jsx'
 import '@/assets/styles/index.css'
 
-// Clear SW lama yang tidak punya denylist
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.getRegistrations().then((registrations) => {
-    registrations.forEach((reg) => reg.unregister())
-  })
-  caches.keys().then((keys) => {
-    keys.forEach((key) => caches.delete(key))
-  })
+async function initApp() {
+  // Unregister SW lama dulu, tunggu selesai
+  if ('serviceWorker' in navigator) {
+    const registrations = await navigator.serviceWorker.getRegistrations()
+    await Promise.all(registrations.map((reg) => reg.unregister()))
+    const keys = await caches.keys()
+    await Promise.all(keys.map((key) => caches.delete(key)))
+  }
+
+  // Baru register SW baru
+  registerSW({ immediate: true })
+
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
 }
 
-registerSW({ immediate: true })
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+initApp()
